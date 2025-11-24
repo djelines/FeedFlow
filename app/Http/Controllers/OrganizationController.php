7f1ad0ac63404f3f7cf7df53d83a9767e414c886
Inès <charines@outlook.fr>
2025-11-24 14:10:30 +0100

    Add create organizations

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Organization\StoreOrganization;
+use App\Actions\Organization\StoreOrganizationAction;
+use App\DTOs\OrganizationDTO;
+use Illuminate\Http\JsonResponse;
 
 class OrganizationController extends Controller
 {
-    //
+
+    public function view(){
+        return view('organizations.index');
+    }
+
+    // Store a newly created organization
+    public function store(StoreOrganization $request, StoreOrganizationAction $action): JsonResponse    {
+
+        // Validate the request and create DTO
+        $dto = OrganizationDTO::fromRequest($request);
+
+        // Execute the action to store the organization
+        $action = new StoreOrganizationAction();
+        $organization = $action->execute($dto);
+
+        return response()->json($organization, 201);
+    }
+
 }
