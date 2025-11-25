41d6f5e8676f3ef5407181949dcd522970c1c52d
zinackes <zinacke@mail.com>
2025-11-25 16:33:28 +0100

    Add range choice in survey answers

--- resources/views/surveys/answer/survey.blade.php
+++ resources/views/surveys/answer/survey.blade.php
@@ -1,24 +1,19 @@
 <x-app-layout>
 
     @php
-        // 1. PRÉPARATION DE LA STRUCTURE (PHP)
-        // On crée un tableau associatif (Clé => Données) pour faciliter la manipulation dans la vue.
-        // Alpine.js utilisera cet objet pour savoir rapidement quelle question mettre à jour.
         $initialState = [];
 
         foreach ($surveyQuestions as $question) {
             $key = 'q_' . $question->id;
             $oldVal = old('questions.' . $key);
 
-            // Gestion spécifique pour checkbox (doit être un tableau)
             if ($question->question_type === 'checkbox' && !is_array($oldVal)) {
                 $oldVal = [];
             }
 
-            // Voici la structure exacte d'un élément de votre tableau final
             $initialState[$key] = [
                 'question_id' => $question->id,
-                'survey_id'   => $question->survey_id, // Ou $survey->id si disponible
+                'survey_id'   => $question->survey_id,
                 'type'        => $question->question_type,
                 'response'    => $oldVal
             ];
@@ -56,17 +51,6 @@
             </div>
         @endif
 
-
-            @error('answers')
-            <p class="text-red-600 text-xs mt-1 font-semibold">{{ $message }}</p>
-            @enderror
-            @error('survey_id')
-            <p class="text-red-600 text-xs mt-1 font-semibold">{{ $message }}</p>
-            @enderror
-            @error('user_id')
-            <p class="text-red-600 text-xs mt-1 font-semibold">{{ $message }}</p>
-            @enderror
-
         <form method="POST" action="{{ route('survey.store.answers') }}">
             @csrf
 
@@ -88,7 +72,6 @@
                         </label>
 
                         <div>
-                            {{-- TYPE: TEXTE --}}
                             @if ($question->question_type === 'text')
                                 <input type="text"
                                        class="w-full border rounded p-2"
@@ -97,7 +80,6 @@
                                        name="questions[{{ $key }}]"
                                        x-on:input="updateResponse('{{ $key }}', $event.target.value)">
 
-                                {{-- TYPE: RADIO --}}
                             @elseif ($question->question_type === 'single_choice' && is_array($question->options))
                                 @foreach ($question->options as $option)
                                     <div class="flex items-center mb-2">
@@ -110,7 +92,6 @@
                                     </div>
                                 @endforeach
 
-                                {{-- TYPE: CHECKBOX --}}
                             @elseif ($question->question_type === 'multiple_choice' && is_array($question->options))
                                 @php $sel = is_array($oldValue) ? $oldValue : []; @endphp
                                 @foreach ($question->options as $option)
@@ -128,8 +109,10 @@
                                        class="w-full"
                                        min="0"
                                        max="10"
-                                       value="0"
-                                       name="questions[{{ $key }}]">
+                                       step="1"
+                                       name="questions[{{ $key }}][]"
+                                       x-on:change="updateResponse('{{ $key }}', $event.target.value)">
+
                             @endif
                         </div>
                     </div>
