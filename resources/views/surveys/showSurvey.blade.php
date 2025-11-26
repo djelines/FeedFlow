86b2c36d2a9985973f391c2be5a19d0ee4528376
xhuriken <celestin@honvault.com>
2025-11-26 21:04:31 +0000

    EDIT: replace icons for font awesome, change hoover

--- resources/views/surveys/showSurvey.blade.php
+++ resources/views/surveys/showSurvey.blade.php
@@ -218,30 +218,40 @@
                             @can('createQuestion', $survey)
                                 <div class="flex flex-col gap-2">
 
+                                    {{-- Edit question --}}
                                     <div class="ml-auto">
                                         <button
                                             @click="openEditModal({{ json_encode($question) }}, '{{ route('survey.question.update', $question) }}')"
-                                            class="p-2 text-gray-400 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-200 rounded-full hover:bg-blue-50 dark:hover:bg-blue-900/20"
-                                            title="Modifier la question">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                                <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                                                <path
-                                                    d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
-                                            </svg>
+                                            class="p-2 rounded-lg
+                                                   text-text-secondary dark:text-text-secondary-dark
+                                                   transition-all duration-150
+                                                   hover:text-primary dark:hover:text-primary-dark
+                                                   hover:scale-110 hover:shadow-md"
+                                            title="Modifier la question"
+                                        >
+                                            <i class="fa-solid fa-pen-to-square text-[15px]"></i>
                                         </button>
                                     </div>
+
+                                    {{-- Delete question --}}
                                     <div class="ml-auto">
-                                        <form action="{{ route('survey.question.destroy', $question) }}" method="POST"
-                                            onsubmit="return confirm('Êtes-vous sûr ?');">
-                                            @csrf @method('DELETE')
-                                            <button type="submit"
-                                                class="p-2 text-gray-400 hover:text-red-600 rounded-full hover:bg-red-50">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
+                                        <form
+                                            action="{{ route('survey.question.destroy', $question) }}"
+                                            method="POST"
+                                            onsubmit="return confirm('Êtes-vous sûr ?');"
+                                        >
+                                            @csrf
+                                            @method('DELETE')
+                                            <button
+                                                type="submit"
+                                                class="p-2 rounded-lg
+                                                       text-text-secondary dark:text-text-secondary-dark
+                                                       transition-all duration-150
+                                                       hover:text-red-500 dark:hover:text-red-400
+                                                        hover:scale-110 hover:shadow-md"
+                                                title="Supprimer la question"
+                                            >
+                                                <i class="fa-solid fa-trash text-[15px]"></i>
                                             </button>
                                         </form>
                                     </div>
